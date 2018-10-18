ac58d6a4e609c3f2485baf3911c403a0ea63ecd2
Guilherme Garcia <guilherme.garcia86@gmail.com>
2018-10-18 20:51:34 -0300

    first commit

---
+++ src/main/java/br/com/guilherme/garcia/financialservice/FinancialServiceApplication.java
@@ -0,0 +1,12 @@
+package br.com.guilherme.garcia.financialservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialServiceApplication.class, args);
+	}
+}
